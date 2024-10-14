998f48a38bc517146140153f696617f5b6af4703
Rayzen7 <nugrahasetio6@gmail.com>
2024-10-14 21:00:44 +0700

    update

---
+++ JavaScript/pembelian.js
@@ -0,0 +1,23 @@
+document.querySelector('.search-button').addEventListener('click', function() {
+    const searchQuery = document.querySelector('.input-container input').value.toLowerCase();
+    const products = document.querySelectorAll('.product-item');
+    const alert = document.querySelector('.notFound');
+
+    let found = false;
+    
+    products.forEach(product => {
+        const productName = product.getAttribute('data-name').toLowerCase();
+        if (productName.includes(searchQuery)) {
+            product.style.display = 'flex';
+            found = true;
+        } else {
+            product.style.display = 'none';            
+        }
+    });
+
+    if (found) {
+        alert.style.display = 'none'; 
+    } else {
+        alert.style.display = 'block';
+    }
+});
